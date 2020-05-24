e2a297eedae65a902b877e7be53febb7ba182e98
Christoph Saile <christoph.saile@googlemail.com>
2020-05-24 17:43:28 +0200

    add: test

--- serviceWorker.js
+++ serviceWorker.js
@@ -17,31 +17,31 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
-// self.addEventListener('fetch', async e => {
-//   const req = e.request;
-//   const url = new URL(req.url);
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
 
-//   if (url.origin === location.origin) {
-//     e.respondWith(cacheFirst(req));
-//   } else {
-//     e.respondWith(networkAndCache(req));
-//   }
-// });
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
 
-// async function cacheFirst(req) {
-//   const cache = await caches.open(cacheName);
-//   const cached = await cache.match(req);
-//   return cached || fetch(req);
-// }
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
 
-// async function networkAndCache(req) {
-//   const cache = await caches.open(cacheName);
-//   try {
-//     const fresh = await fetch(req);
-//     await cache.put(req, fresh.clone());
-//     return fresh;
-//   } catch (e) {
-//     const cached = await cache.match(req);
-//     return cached;
-//   }
-// }
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
